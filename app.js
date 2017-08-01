6ee2619f4bb5af87b92e20e83bc7f2b62bfd8e24
Evan Freeze <evanfreeze@gmail.com>
2017-07-31 19:51:31 -0500

    Adds basic timer functionality

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let isRunning;
+let timerLength = '25';
+let breakLength;
+$('span').text(timerLength);
 
 const setTimerLength = value => value;
 
@@ -20,9 +23,7 @@ const showTimerLengthMinutesSeconds = (value) => {
 
 const decrementTimer = (timerValue, amountToDecrement) => timerValue - amountToDecrement;
 
-const updateTimerView = (valueToShow) => {
-  $('p').html(valueToShow);
-};
+const updateTimerLengthDisplay = newValue => $('span').html(newValue);
 
 const updateLabelView = (valueToShow) => {
   $('h2').html(valueToShow);
@@ -30,13 +31,14 @@ const updateLabelView = (valueToShow) => {
 
 const startTimerAtValue = (timerValue) => {
   let myTimer = timerValue;
+  console.log(myTimer);
   let timeString = showTimerLengthMinutesSeconds(myTimer);
-  updateTimerView(timeString);
+  updateTimerLengthDisplay(timeString);
   setTimerLength(myTimer);
   const timer = setInterval(() => {
     myTimer = decrementTimer(myTimer, 1000);
     timeString = showTimerLengthMinutesSeconds(myTimer);
-    updateTimerView(timeString);
+    updateTimerLengthDisplay(timeString);
     if (myTimer === 0) {
       clearInterval(timer);
       setTimeout(() => {
@@ -51,8 +53,47 @@ const startTimerWithBreak = (timerValue, breakValue) => {
   startTimerAtValue(timerValue);
 };
 
+const convertMinStringToMs = (minutesString) => {
+  const minutes = parseInt(minutesString, 10);
+  const seconds = minutes * 60;
+  const milseconds = seconds * 1000;
+  return milseconds;
+};
+
+const getSpecifiedTimerLength = () => convertMinStringToMs(timerLength);
+
+const getSpecifiedBreakLength = () => {
+
+};
+
+const alterTimerLength = (operator, amountToAlter) => {
+  let length = getSpecifiedTimerLength();
+  console.log(length);
+  const minutesToAlter = amountToAlter * 60000;
+  console.log(minutesToAlter);
+  if (operator === '+') {
+    length += minutesToAlter;
+  } else {
+    length -= minutesToAlter;
+  }
+  console.log(length);
+  const minuteLength = length / 1000 / 60;
+  console.log(minuteLength);
+  timerLength = minuteLength;
+  updateTimerLengthDisplay(timerLength);
+};
+
 $('document').ready(() => {
   // startTimerWithBreak(12000, 90000);
+  $('button').click(() => {
+    startTimerAtValue(getSpecifiedTimerLength());
+  });
+  $('.fa-plus').click(() => {
+    alterTimerLength('+', 1);
+  });
+  $('.fa-minus').click(() => {
+    alterTimerLength('-', 1);
+  });
 });
 
 // module.exports.setTimerLength = setTimerLength;
